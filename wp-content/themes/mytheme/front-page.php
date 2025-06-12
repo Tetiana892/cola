a5879956ffbc0dd1e947120d80618ab23a685221
Tetiana <onyshch@ukr.net>
2025-06-12 18:44:34 +0200

    add style mobile

--- wp-content/themes/mytheme/front-page.php
+++ wp-content/themes/mytheme/front-page.php
@@ -5,13 +5,19 @@
 		<h1 class="hero-title">Main Site Title</h1>
 		<h2 class="hero-info">Lorem ipsum descripsum</h2>
 		<button class="hero-btn">Call to action</button>
-		<div class="hero-images">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/snow.png" alt="snow" class="img-snow">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/Cloud.png" alt="cloud" class="img-cloud">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/dedcoca.png" alt="dedcola" class="img-dedcola">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/arrow.png" alt="arrow" class="img-arrow">
-		</div>
 	</div>
+	<img src="<?php echo get_template_directory_uri(); ?>/images/snow.png" alt="snow" class="img-snow">
+	<img src="<?php echo get_template_directory_uri(); ?>/images/dedcoca.png" alt="dedcola" class="img-dedcola">
+	<img src="<?php echo get_template_directory_uri(); ?>/images/arrow.png" alt="arrow" class="img-arrow">
+	<img src="<?php echo get_template_directory_uri(); ?>/images/dedcola-mobile.png" alt="dedcola mobile" class="img-dedcola-mobile">
+
+	<div class="img-cloud-wrapper">
+		<img src="<?php echo get_template_directory_uri(); ?>/images/Cloud.png" alt="cloud" class="img-cloud">
+		<p class="hero-say">How how how !!!<br>
+			Like it?<br>
+			Scroll down for more !</p>
+	</div>
+
 </section>
 
 <?php get_footer(); ?>
